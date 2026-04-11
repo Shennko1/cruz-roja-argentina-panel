3c7c301ec75f48850624216e1aa42516473bbe70
Shennko1 <102772426+Shennko1@users.noreply.github.com>
2026-04-11 00:44:09 -0300

    Create page.js

---
+++ app/visor-mendoza/page.js
@@ -0,0 +1,17 @@
+export default function VisorMendoza() {
+  return (
+    <div className="min-h-screen bg-[#f4f4f4] p-8">
+      <h1 className="text-2xl font-bold mb-4">
+        Visor de Alertas y Emergencias - Mendoza
+      </h1>
+
+      <iframe
+        src="https://lookerstudio.google.com/embed/reporting/db07018b-18ed-4f61-b516-8f2f96cbe56d/page/jO2jF"
+        width="100%"
+        height="800"
+        style={{ border: 0 }}
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
