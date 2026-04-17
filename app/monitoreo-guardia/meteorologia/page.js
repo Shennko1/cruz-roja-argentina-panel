a8c10079d0fd5de3f301431c57653414fba79474
Shennko1 <102772426+Shennko1@users.noreply.github.com>
2026-04-17 00:29:38 -0300

    Update page.js

--- app/monitoreo-guardia/meteorologia/page.js
+++ app/monitoreo-guardia/meteorologia/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ModuloEnConstruccion() {
   return (
     <div className="bg-white p-8 rounded-xl border border-gray-200 shadow-sm mt-4 text-center">
-    <p className="text-gray-500 font-medium"> Visor operativo en configuración. </p>
+      <p className="text-gray-500 font-medium">Visor operativo en configuración.</p>
     </div>
   );
-} 
+}
